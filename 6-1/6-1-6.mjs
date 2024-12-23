b6fcb8775b4c7b3c8e682783c405237b0b31c4c2
hc421004 <nkhshc421004@g.neec.ac.jp>
2024-12-23 11:43:05 +0900

    6-1-1

---
+++ 6-1/6-1-6.mjs
@@ -0,0 +1,11 @@
+const a = 10;
+
+console.log(a > 9)
+console.log(a >= 10)
+console.log(a < 11)
+console.log(a <= 10)
+
+console.log(a > 11)
+console.log(a >= 11)
+console.log(a < 9)
+console.log(a <= 9)
---
+++ 6-1/6-1-7.mjs
@@ -0,0 +1,5 @@
+const a = 10;
+
+console.log(a === 11)
+console.log(a === "10")
+console.log(a === a)
