0044bb7b8f77aba310c25b001148b031cb5d19a8
hc421004 <nkhshc421004@g.neec.ac.jp>
2024-12-23 16:19:52 +0900

    6-6-1

---
+++ 6-6/6-6-3.mjs
@@ -0,0 +1,13 @@
+const map6_6_3 = new Map()
+map6_6_3.set("A", "カレー")
+map6_6_3.set("B", "定食")
+
+console.log(map6_6_3.has("A"))
+console.log(map6_6_3.get("A"))
+
+console.log(map6_6_3.has("B"))
+console.log(map6_6_3.get("b"))
+
+for (const [key, value] of map6_6_3) {
+    console.log(key, value)
+}
---
+++ 6-6/6-6-5.mjs
@@ -0,0 +1,7 @@
+const bestObject6_5_5 = new Map()
+bestObject6_5_5.set("A", "カレー")
+bestObject6_5_5.set("B", "定食")
+
+for (const [key, value] of bestObject6_5_5) {
+    console.log(key, value)
+}
