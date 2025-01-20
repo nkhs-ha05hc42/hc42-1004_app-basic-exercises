f1234f59553b7f744e6826518f90984666776103
hc421004 <nkhshc421004@g.neec.ac.jp>
2025-01-20 15:53:28 +0900

    7-2

---
+++ 7-2/7-2-3_export.mjs
@@ -0,0 +1,6 @@
+export const calc1 = (a,b) => {
+    return a * b
+}
+export const calc2 = (a,b) => {
+    return a / b
+}
---
+++ 7-2/7-2-4_import.mjs
@@ -0,0 +1,7 @@
+import {calc1} from "./7-2-4_kake_export.mjs"
+import {calc2} from "./7-2-4_ wari_export.mjs"
+
+const result1 = calc1(3,5)
+console.log("calc1",result1)
+const result2 = calc2(3,5)
+console.log("calc",result2)
